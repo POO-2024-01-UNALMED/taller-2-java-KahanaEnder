8e88d74e521a8abd6b61c85205a7d98f0e01658a
Juan José <142289031+KahanaEnder@users.noreply.github.com>
2024-02-20 10:53:56 -0500

    Taller

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,35 +1,37 @@
 package test;
+
 public class Auto {
-    String modelo;
-    int precio;
-    Asiento [] asientos;
-    String marca;
-    Motor motor;
-    int registro;
-    static int cantidadcreados;
+String modelo;
+int precio;
+Asiento [] asientos;
+String marca;
+Motor motor;
+int registro;
+static int cantidadCreados;
 
-    int CantidadAsientos() {
-        int QuantityAsientos  = 0;
-        for (int i = 0; i < asientos.length ; i++) {
-            if (asientos[i] != null) {
-                QuantityAsientos++;
-            }
+int cantidadAsientos() {
+int cantAsientos = 0;
+for(int i = 0; i < asientos.length ; i++) {
+if(asientos[i] != null) {
+cantAsientos++;
+}
+}
+return cantAsientos;
+}
 
+String verificarIntegridad() {
+if(this.registro == this.motor.registro) {
+for(int i = 0 ; i < asientos.length ; i++) {
+if(asientos[i] != null && asientos[i].registro != registro) {
+return "Las piezas no son originales";
+}
+}
 
-    }
-        return QuantityAsientos;
-    }
-String verificarIntegridad(){
-    if (this.registro == this.motor.registro) {
-        for (int i = 0; i < asientos.length ; i++) {
-            if (asientos[i] != null && asientos[i].registro != registro) {
-                return "Las piezas no son originales";
-        }
-    }
-    return "Auto original";
+return "Auto original";
 
 } else {
-    return "Las piezas no son originales";
+return "Las piezas no son originales";
 }
 }
+
 }
--- src/test/java/test/Motor.java
+++ src/test/java/test/Motor.java
@@ -1,17 +1,24 @@
 package test;
-public class Motor {
-    public static void main(String[] args) {
 
-    }
-    int numeroCilindros;
-    String tipo;
-    int registro;
-    void CambiarRegistro(int registro) {
-        this.registro = registro;
-    }
-    void asignarTipo(String tipo) {
-        if (tipo == "electrico" || tipo == "gasolina") {
-            this.tipo = tipo;
-        }
+
+
+public class Motor {
+public static void main(String args[]) {
+
 }
+
+int numeroCilindros;
+String tipo;
+int registro;
+
+void cambiarRegistro(int registro) {
+this.registro = registro;
+}
+
+void asignarTipo(String tipo) {
+if (tipo == "electrico" || tipo == "gasolina") {
+this.tipo = tipo;
+}
+}
+
 }
